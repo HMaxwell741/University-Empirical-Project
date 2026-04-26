85e627cdd83ec3c7c44a66a12dea760a72dfd43d
HMaxwell741 <hrm226@exeter.ac.uk>
2026-04-26 08:00:59 +0100

    Adding make script when all 3 scripts are to run

---
+++ Make_with_API.py
@@ -0,0 +1,24 @@
+print('Installing Libraries')
+
+import subprocess
+import papermill as pm
+
+subprocess.check_call(['pip', 'install','papermill','requests', 'pandas','beautifulsoup4','sentence-transformers','python-dateutil', 'newsapi-python','python-dotenv', 'yfinance','transformers', 'torch','statsmodels', 'matplotlib', 'scikit-learn', 'scipy',
+    'huggingface_hub', 'folium','jupyter'])
+
+print('Libraries all installed')
+print('Running Scripts, will update after each script complete, this may take a couple of minutes (see README)')
+
+pm.execute_notebook('Scripts/Scraping.ipynb', cwd='Scripts')
+
+print('Scraping complete')
+
+pm.execute_notebook('Scripts/Sentiment.ipynb', cwd='Scripts')
+
+print ('Sentiment complete')
+
+pm.execute_notebook('Scripts/Output.ipynb', cwd='Scripts')
+
+print('outputs complete')
+
+print('All scripts executed')
